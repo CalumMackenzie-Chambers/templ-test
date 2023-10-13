12f194844bc9c6a6bd377ddcfb1977605f9cda18
Calum <calum@mackenzie-chambers.co.uk>
2023-10-13 02:30:36 +0100

    feat: add tailwind to basic component

--- cmd/main.go
+++ cmd/main.go
@@ -6,15 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/CalumMackenzie-Chambers/templ-test/server"
-	"github.com/CalumMackenzie-Chambers/templ-test/templates/components"
+	"github.com/CalumMackenzie-Chambers/templ-test/templates/layouts"
 )
 
 func main() {
 	r := gin.Default()
 	r.HTMLRender = &server.TemplRender{}
+	r.Static("/static", "./static")
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index", components.Hello("Calum"))
+		c.HTML(http.StatusOK, "index", layouts.BaseLayout())
 	})
 
 	r.Run(":8080")
